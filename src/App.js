70c87173294a9e3e8c74c73b780deca5c792cbee
Robby Ray <rgr5035@gmail.com>
2021-02-23 12:38:09 -0700

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    return (
-      <Router> 
-        <div>
-          <Navbar />
-            
-              <Route exact path ="/" component={Home}/>
-            
-        </div>
-      </Router> 
-    ) 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
