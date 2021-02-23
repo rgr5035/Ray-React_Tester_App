c6b0f2948258ca963d94b8672fcbbf2b42c14125
Robby Ray <rgr5035@gmail.com>
2021-02-23 13:06:02 -0700

    added notes

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
 console.log(element);
+
+//first argument is the element we want to render, second parameter specify where in realDOM we want to render
+ReactDOM.render(element, document.getElementById("root"));
